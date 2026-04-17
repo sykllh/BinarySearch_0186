780e83452a1d318f5fe868b456c4931d8a855b6e
sykllh <sykllhidmi@gmail.com>
2026-04-17 15:10:37 +0700

    membuat fungsi dengan binary Search

--- BinarySort.cpp
+++ BinarySort.cpp
@@ -58,4 +58,15 @@ void display()
     cout << "\n====================================\n";
 
     for (int j = 0; j < nPanjang; j++)
+    {
+        cout << element[j];
+        if (j < nPanjang - 1)
+        {
+            cout << " -> ";
+        }
+    }
+    cout << endl;
 }
+
+void binarySearch()
+{}

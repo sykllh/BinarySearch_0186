c844041705229d7a8d6a32df0305d0c729d5a852
sykllh <sykllhidmi@gmail.com>
2026-04-17 14:55:40 +0700

    membuat fungsi dengan bubblesortarray

--- BinarySort.cpp
+++ BinarySort.cpp
@@ -27,4 +27,10 @@ void input()
     cout << "\n====================================\n";
 
     for (int i = 0; i < nPanjang; i++)
+    {
+        cout << "Data ke-" << (i + 1) << "  = ";
+        cin >> element[i];
+    }
 }
+
+void bubbleSortArray()
